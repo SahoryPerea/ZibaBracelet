c53cd0cb8690a394efff6f8409c3372ccc9320fd
SahoryPerea <140366489+SahoryPerea@users.noreply.github.com>
2023-07-24 08:47:13 -0600

    Add files via upload

---
+++ php/conexion_be.php
@@ -0,0 +1,14 @@
+<?php
+    $conexion = mysqli_connect ("localhost", "root", "", "login_register_db");
+
+    /*
+    if($conexion){
+        echo 'Conectado correctamente a la base de datos';
+    }
+
+    else{
+        echo 'No se ha podido conectar a la base de datos';
+    }
+    */
+    
+?>
